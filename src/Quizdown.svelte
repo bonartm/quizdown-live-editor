8f4759ffea9c3a2db47ef79923e70eb4ee91fbd1
bonartm <malte@spiced-academy.com>
2021-04-15 15:05:11 +0200

    use bind:this

--- src/Quizdown.svelte
+++ src/Quizdown.svelte
@@ -3,27 +3,23 @@
 import { onMount } from 'svelte';
 import { create_app } from 'quizdown'
 import 'quizdown/public/build/quizdown.css';
-import { create_bidirectional_transition } from 'svelte/internal';
 
 export let code: string;
-export let id: string;
 
+let quizdown_node;
 
-let node;
-
-onMount(async () => { 
-    node = document.getElementById(id)   
-    create_app(code, node)
+onMount(async () => {     
+    create_app(code, quizdown_node, {})
 });
 
 $: {
     if (document.readyState === "complete"){
-        create_app(code, node)
+        create_app(code, quizdown_node, {})
     }
 }
 </script>
 
-<div id={id} class='quizdown-area'></div>
+<div bind:this={quizdown_node} class='quizdown-area'></div>
 
 <style>
     .quizdown-area {
